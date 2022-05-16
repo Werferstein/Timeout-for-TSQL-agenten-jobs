9546d5abdd2c241a533dbf42cfefb0d9e02678e8
Bluefrog <code@Werferstein.org>
2022-05-16 17:14:44 +0200

    Update JobTimeout.sql

--- JobTimeout.sql
+++ JobTimeout.sql
@@ -12,8 +12,8 @@ Wenn das Timeout überschritten wurde, wird der betreffende Job gestoppt und ein
 DECLARE 
 @job_name NVARCHAR(MAX) = 'Agenten Job Name',
 @TimeOutInMin INT		    = 60,
-@EmailAdresse1 NVARCHAR(200)	    " 'YourEmail.com',
-@EmailProfil NVARCHAR(200)	    " 'YourEmailProfil'
+@EmailAdresse1 NVARCHAR(200)	    = 'YourEmail.com',
+@EmailProfil NVARCHAR(200)	    = 'YourEmailProfil'
 
 ------------------------------------------------------------------------------------------------------------------------------------------
 
